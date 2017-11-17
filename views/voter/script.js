faed86460a0fa2d02f993baa28df0f3abc627634
Aleita <traina@oregonstate.edu>
2017-11-16 17:12:44 -0800

    Feature (#3)
    * Login page
    
    * styles and additions
    
    * Ballot page can now display candidates based on voter and store form values of selected candidates in script.js file

---
+++ views/voter/script.js
@@ -0,0 +1,32 @@
+
+
+var voter = {
+    authToken: "0111US890",
+    address: "2500 Monroe Avenue, Corvallis, Oregon",
+    fName: "Rini",
+    lName: "Chatterjee"
+};
+
+var ballot = {
+    authToken: "0111US890",
+    Ofc1: "Mayor",
+    Ofc1Cnd1: "Aleita Train, Green Party",
+    Ofc1Cnd2: "Katie Beth, Democratic Party",
+    Ofc2: "Counsellor",
+    Ofc2Cnd1: "Matt Castillo, Communist Party",
+    Ofc2Cnd2: "Sean Solberg, Socialist Party"
+};
+
+document.getElementById("voterName").innerHTML = voter.fName + ' ' + voter.lName;
+document.getElementById("voterAddress").innerHTML = voter.address;
+document.getElementById("voterAuthToken").innerHTML = voter.authToken;
+
+document.getElementById("Ofc1").innerHTML = ballot.Ofc1;
+
+document.getElementById("Ofc1Cnd1").innerHTML = ballot.Ofc1Cnd1;
+document.getElementById("Ofc1Cnd2").innerHTML = ballot.Ofc1Cnd2;
+
+function test() {
+    // var x = document.getElementById("postBallot").elements[0].value;
+    console.log(document.getElementById("postBallot").elements[0].value);
+};
